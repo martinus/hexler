1c64c2b794dbc97047dfb38cb64f5a5cd7fb64fd
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2024-02-04 13:04:56 +0100

    adds testdata

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use std::{fs::File, io::Write};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
 }
 
+fn create_binary_file() -> std::io::Result<()> {
+    let mut file = File::create("bytes.bin")?;
+
+    // Create a byte array with values from 0 to 255
+    let bytes: Vec<u8> = (0..=255).collect();
+
+    // Write the byte array to the file
+    file.write_all(&bytes)?;
+
+    Ok(())
+}
+
+fn main() {
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
+
+    match create_binary_file() {
+        Ok(()) => println!("Binary file created successfully!"),
+        Err(err) => println!("Error creating file: {}", err),
+    }
+    println!("Hello, world!");
+}
