85f7bcb94fcbf0632d6d244bfb2f74eb2bf56f4f
Martin Leitner-Ankerl <martin.ankerl@gmail.com>
2024-03-03 18:58:01 +0100

    works, but  looks a bit complicated

--- src/test.cpp
+++
@@ -1,4 +0,0 @@
-// calculate gcd in constexpr, in c++17, but not recursively.
-constexpr int gcd(int a, int b) {
-    writer: std::io::BufWriter<std::io::Stdout>,
-    
---
+++ tests/data/test.py
@@ -0,0 +1,15 @@
+import sys
+
+for code in range(30, 38):
+    sys.stdout.write(f"\u001b[{code}m{code} ")
+print()
+for code in range(90, 98):
+    sys.stdout.write(f"\u001b[{code}m{code} ")
+sys.stdout.write(f"\u001b[0m0 ")
+print()
+
+for i in range(0, 16):
+    for j in range(0, 16):
+        code = str(i * 16 + j)
+        sys.stdout.write(f"\u001b[38;5;{code}m{code.rjust(5)}")
+    print("\u001b[0m")
